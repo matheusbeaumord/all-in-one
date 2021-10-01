9fbcf0fc3e56beeb63929ae067e1bdbf57f11a32
Matheus Beaumord <matheusbeaumord@gmail.com>
2021-10-01 11:31:45 -0300

    feat: estruturaçao do projeto

---
+++ src/http-client/index.tsx
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const httpClient = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com/',
+});
+
+httpClient.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
